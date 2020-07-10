7857d274beadf42b4a25975392abddfcefcafd61
toyoka14 <toyoka14@vivaldi.net>
2020-07-10 21:10:52 +0900

    Update: rails tutorial ch07 7.3.4

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,13 +5,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
 
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name: "",
+      post signup_path, params: { user: { name: "",
                                          email: "user@invalid",
                                          password: "foo",
                                          password_confirmation: "bar"}}
     end
     assert_template 'users/new'
-    assett_select 'div#<CSS id for error explanation>'
-    assett_select 'diev.<CSS class for field with error>'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert' do 'div.alert-danger' end
+    assert_select 'form[action="/signup"]'
   end
 end
