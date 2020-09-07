8bd06b860cc4b84c7c65c3551c68f2b3f22fdd5a
toyoka14 <toyoka14@vivaldi.net>
2020-09-08 08:33:08 +0900

    Update: railstutorial ch12 12.3.1

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,7 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user,   only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+
   def new
   end
 
@@ -8,6 +11,7 @@ class PasswordResetsController < ApplicationController
       @user.create_reset_digest
       @user.send_password_reset_email
       flash[:info] = "Email sent with password reset instructions"
+      redirect_to root_url
     else
       flash.now[:danger] = "Email address not found"
       render 'new'
@@ -16,4 +20,15 @@ class PasswordResetsController < ApplicationController
 
   def edit
   end
+
+  private
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    def valid_user
+      unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
+    end
 end
